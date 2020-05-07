fe17d69bfdc2e609f351ceaa7fb667c48c96b654
olivercastroteran <olivercastroteran@gmail.com>
2020-05-07 02:27:24 -0400

    figcaption added to img-container

--- js/app.js
+++ js/app.js
@@ -8,34 +8,3 @@ window.addEventListener('scroll', function () {
     document.getElementById('sec-nav').classList.remove('active');
   }
 });
-
-let contArr = [...document.querySelectorAll('.img-container')];
-
-contArr.map((img, index) => {
-  img.addEventListener('mouseover', () => {
-    let descArr = [...document.querySelectorAll('.description')];
-    for (let i = 0; i < descArr.length; i++) {
-      if (index === i) {
-        descArr[i].classList.add('active');
-      }
-    }
-    let box = document.querySelector('.description.active');
-    box.addEventListener('mouseover', () => {
-      img.firstElementChild.style.filter = 'blur(3px)';
-      img.firstElementChild.style.opacity = '1';
-    });
-    box.addEventListener('mouseout', () => {
-      img.firstElementChild.style.filter = 'blur(0px)';
-      img.firstElementChild.style.opacity = '0.5';
-    });
-  });
-});
-
-contArr.map((img) => {
-  img.addEventListener('mouseout', () => {
-    let descArr = [...document.querySelectorAll('.description')];
-    for (let i = 0; i < descArr.length; i++) {
-      descArr[i].classList.remove('active');
-    }
-  });
-});
