254a25afcc144ff6f13e2ce6f0284473d9c18efe
Kurt Guardia <kurtguardia@gmail.com>
2020-05-12 21:45:38 +0200

    Made optimizations in all pages meta tags, images, anchor tags, etc

--- js/app-cinll.js
+++ js/app-cinll.js
@@ -1,25 +1,27 @@
+
+
+//_________________Hamburger Navbar___________________
 const navSlide = () => {
-    const burger2 = document.querySelector('.burger2');
-    const navMini = document.querySelector('.nav-mini');
-    const navLinksMini = document.querySelectorAll('.nav-mini li');
-  
-    // Toggle NavBar Mini
-    burger2.addEventListener('click', () => {
-      navMini.classList.toggle('nav-active');
-      // Animate links
-      navLinksMini.forEach((link, index) => {
-        if (link.style.animation) {
-          link.style.animation = '';
-        } else {
-          link.style.animation = `navLinkFade 0.5s ease forwards ${
-            index / 7 + 0.4
-          }s`;
-        }
-      });
-      // Burger Animation
-      burger2.classList.toggle('toggle');
+  const burger2 = document.querySelector(".burger2");
+  const navMini = document.querySelector(".nav-mini");
+  const navLinksMini = document.querySelectorAll(".nav-mini li");
+
+  // Toggle NavBar Mini
+  burger2.addEventListener("click", () => {
+    navMini.classList.toggle("nav-active");
+    // Animate links
+    navLinksMini.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.4
+        }s`;
+      }
     });
-  };
-  
-  navSlide();
-  
+    // Burger Animation
+    burger2.classList.toggle("toggle");
+  });
+};
+
+navSlide();
